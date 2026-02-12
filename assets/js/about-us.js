9f2e11807dc96d1e5f36e2d55e48da39868afe99
kaashutosh <aashutosh21.k@gmail.com>
2026-02-12 21:18:09 +0530

    responsive changes

--- assets/js/about-us.js
+++ assets/js/about-us.js
@@ -1,22 +1,22 @@
 document.getElementById("mobileMenuBtn").onclick = () => {
-            document.getElementById("mobileNav").classList.toggle("hidden");
-        };
+    document.getElementById("mobileNav").classList.toggle("hidden");
+};
 
-        // Mobile diabetes open/close
-        document.getElementById("diabetesBtn").onclick = () => {
-            document.getElementById("diabetesMenu").classList.toggle("hidden");
-        };
+// Mobile diabetes open/close
+document.getElementById("diabetesBtn").onclick = () => {
+    document.getElementById("diabetesMenu").classList.toggle("hidden");
+};
 
-        // Desktop smooth menu fix
-        let desktopTimer = null;
+// Desktop smooth menu fix
+let desktopTimer = null;
 
-        function openDesktopMenu() {
-            clearTimeout(desktopTimer);
-            document.getElementById("desktopDiabetesMenu").classList.remove("hidden");
-        }
+function openDesktopMenu() {
+    clearTimeout(desktopTimer);
+    document.getElementById("desktopDiabetesMenu").classList.remove("hidden");
+}
 
-        function closeDesktopMenu() {
-            desktopTimer = setTimeout(() => {
-                document.getElementById("desktopDiabetesMenu").classList.add("hidden");
-            }, 200);
-        }
+function closeDesktopMenu() {
+    desktopTimer = setTimeout(() => {
+        document.getElementById("desktopDiabetesMenu").classList.add("hidden");
+    }, 200);
+}
